deca2d67c1353dcc40cc4ebc18f04e0d7010380d
diyorarti <diyorarti@gmail.com>
2025-08-28 08:39:19 +0500

    Hero Section

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,18 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className='text-4xl flex h-screen justify-center items-center '>
-      App
-    </div>
+    <main>
+      <div className='hero'/>
+
+      <div className='hero-details'>
+        <img 
+          src='/hero.png'
+        />
+        <h1>
+          Find <span className='text-gradient'>Movies</span> You'll Enjoy Without the Hassle
+        </h1>
+      </div>
+    </main>
   )
 }
 
